55e49725b2cd4d94742a6741ba720063acb733e9
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-03-25 10:57:11 +0100

    fix: repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,19 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "server",
-
   adapter: cloudflare(),
-
   env: {
     validateSecrets: false,
-
     schema: {
       LIVEKIT_URL: envField.string({ context: "server", access: "secret" }),
     },
   },
-
   vite: {
     build: {
       minify: false,
-    },
-
-    ssr: {
-      external: ["node:crypto"],
-    },
-
-    resolve: {
-      alias: import.meta.env.PROD
-        ? {
-          "react-dom/server": "react-dom/server.edge",
-        }
-        : undefined,
     },
   },
 });
